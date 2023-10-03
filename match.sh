ab1c327a30ab1a10b263a4fa3b0485e1c0fc8249
strangeloop <contact@strangeloop.blog>
2023-10-03 18:11:51 -0400

    checkpoint but need to only capture the foreground OR background where the color matched

--- match.sh
+++ match.sh
@@ -2,12 +2,13 @@
 
 # parse list of all colors that the shiki theme uses
 light_colors=( $(grep -oE '#[0-9A-Fa-f]{6}' ./leuven.json) )
-echo -e "Parsed Colors:\n ${light_colors[*]}"
+#echo -e "Parsed Colors:\n [${light_colors[*]}]"
 # grep for those colors and group by match as there will likely be multiple matches
-keys=( $(grep -E -f "${light_colors[*]}" ./leuven-theme.el) )
-#keys=( $(grep -E -f <(printf "%s\n" "${light_colors[@]}") ./leuven-theme.el ) )
+regex_pattern=$(IFS="|"; echo "${light_colors[*]}")
+keys=( "$(grep -E "$regex_pattern" ./leuven-theme.el)" )
 
-echo -e "Matched Keys:\n ${keys[1]}"
+echo -e "Matched Keys:\n ${keys[*]}"
+	   
 # select the keys to use
 
 # duplicate shiki theme
