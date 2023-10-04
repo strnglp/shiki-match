71515b73e2f5347ed23531dfdf44f5294aa5f085
strangeloop <strangeloop@tower.local>
2023-10-04 13:50:41 -0400

    checkpoint working matches but variable sized between themes, moving to associative arrays next to resolve duplicates

--- match.sh
+++ match.sh
@@ -11,8 +11,12 @@ process_matches() {
     local -n values="$3"
     local fg_or_bg="$4"
     for match in ${matches[@]}; do
-	# split at first :, use the first part as the key as it provides context
-	part1="${match%%:*}"
+	# split at first color definition, use the first part as the key as it provides context
+	if [[ $match == *:foreground* ]]; then
+	    part1="${match%%:foreground*}"
+	else
+	    part1="${match%%:background*}"
+	fi
 	# Trim leading whitespace
 	part1="${part1#"${part1%%[![:space:]]*}"}"
 	# Trim trailing whitespace
@@ -53,37 +57,44 @@ bg_matches=($bg_matches)
 declare -a fg_keys
 declare -a fg_values
 process_matches fg_matches fg_keys fg_values "foreground"
+declare -a bg_keys
+declare -a bg_values
+process_matches bg_matches bg_keys bg_values "background"
 
+
+# foreground matches
+dark_fg_values=()
+for key in ${fg_keys[@]}; do
+    # match each key in the dark-theme file
+    match="$(grep -F $key ./leuven-dark-theme.el)"
+    # parse the foreground color
+    value=$(echo "$match" | grep -oE "foreground\\s*\"#[0-9A-Fa-f]{6}\"" | grep -oE '#[0-9A-Fa-f]{6}')
+    dark_fg_values+=($value)
+done
+
+# check sizes
+echo "${#fg_keys[@]}"
+echo "${#dark_fg_values[@]}"
 
 #end=$(( ${#fg_keys[@]} - 1 ))
 #for i in $(seq 0 $end); do
-#    echo  "${fg_keys[$i]} : ${fg_values[$i]}"
+#    echo "KEY: [${fg_keys[$i]}] LIGHT: [${fg_values[$i]}]  DARK: [${dark_fg_values[$i]}]"
 #done
-#echo "-----------------------------------------------------"
-
-#echo "${fg_keys[*]}"
-# match the keys to the dark version
-regex_pattern=$(IFS="|"; echo "${fg_keys[*]}")
-fg_matches="$(grep -E '"$regex_pattern"' ./leuven-dark-theme.el)"
-
-# missing only the first line? (code-inline '(
-echo  "${fg_matches[*]}"
-
-#end=$(( ${#fg_keys[@]} - 1 ))
-#for i in $(seq 0 $end); do
-#    # match each key in the dark-theme file
-#    # parse the foreground color
-#    # add it to the fg_dark_values
-#done
-
-
-#declare -a bg_keys
-#declare -a bg_values
-#process_matches bg_matches bg_keys bg_values "background"
 #
-#end=$(( ${#bg_keys[@]} - 1 ))
-#for i in $(seq 0 $end); do
-#done
+# bg matches
+dark_bg_values=()
+for key in ${bg_keys[@]}; do
+    # match each key in the dark-theme file
+    match="$(grep -F $key ./leuven-dark-theme.el)"
+    value=$(echo "$match" | grep -oE "background\\s*\"#[0-9A-Fa-f]{6}\"" | grep -oE '#[0-9A-Fa-f]{6}')
+    # parse the foreground color
+    dark_bg_values+=($value)
+done
+
+# check sizes
+echo "${#bg_keys[@]}"
+echo "${#dark_bg_values[@]}"
+
 
 	   
 
