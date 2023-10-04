0f05d482a9020d4d44eb9b095401fa5009a0f43d
strangeloop <strangeloop@thinkpad.local>
2023-10-04 10:52:36 -0400

    checkpoint

--- match.sh
+++ match.sh
@@ -19,12 +19,11 @@ process_matches() {
 	key="${part1%"${part1##*[![:space:]]}"}"
 	# process the second part and extract only the color code as the value
 	part2="${match#*:}"
-	value=$(echo "$part2" | grep -oE 'foreground\s*"#[0-9A-Fa-f]{6}"' | grep -oE '#[0-9A-Fa-f]{6}')
-
+	value=$(echo "$part2" | grep -oE "$fg_or_bg\\s*\"#[0-9A-Fa-f]{6}\"" | grep -oE '#[0-9A-Fa-f]{6}')
 	# add the key
-	keys+=(key)
+	keys+=($key)
 	# add the value
-	values+=(value)
+	values+=($value)
     done
 }
 
@@ -39,14 +38,12 @@ light_colors=( $(grep -oE '#[0-9A-Fa-f]{6}' ./leuven.json) )
 # grep for those colors and group by match
 # as there will likely be multiple matches between fg and bg colors
 # split the search into two distinct outputs, one for each usecase
-fg_search=("${light_colors[@]/#/foreground(\w*) \"}")
-bg_search=("${light_colors[@]/#/background(\w*) \"}")
-fg_regex_pattern=$(IFS="|"; echo "${fg_search[*]}")
-bg_regex_pattern=$(IFS="|"; echo "${bg_search[*]}")
-
-# match fg and bg using the above patterns, grep returns a string
-fg_matches=( "$(grep -E "$fg_regex_pattern" ./leuven-theme.el)" )
-bg_matches=( "$(grep -E "$bg_regex_pattern" ./leuven-theme.el)" )
+search=("${light_colors[@]/#/foreground(\w*) \"}")
+regex_pattern=$(IFS="|"; echo "${search[*]}")
+fg_matches="$(grep -E "$regex_pattern" ./leuven-theme.el)"
+search=("${light_colors[@]/#/background(\w*) \"}")
+regex_pattern=$(IFS="|"; echo "${search[*]}")
+bg_matches="$(grep -E "$regex_pattern" ./leuven-theme.el)"
 
 # split strings into arrays
 IFS=$'\n'
@@ -57,19 +54,43 @@ declare -a fg_keys
 declare -a fg_values
 process_matches fg_matches fg_keys fg_values "foreground"
 
-for k in ${fg_keys[@]}; do
-    echo $k
-done
 
-#echo -e "Matched FG Keys:\n${fg_keys[*]}"
-#echo "==================================="
-#echo -e "Matched BG Keys:\n${bg_keys[*]}"
-#echo "==================================="
+#end=$(( ${#fg_keys[@]} - 1 ))
+#for i in $(seq 0 $end); do
+#    echo  "${fg_keys[$i]} : ${fg_values[$i]}"
+#done
+#echo "-----------------------------------------------------"
+
+#echo "${fg_keys[*]}"
+# match the keys to the dark version
+regex_pattern=$(IFS="|"; echo "${fg_keys[*]}")
+fg_matches="$(grep -E '"$regex_pattern"' ./leuven-dark-theme.el)"
+
+# missing only the first line? (code-inline '(
+echo  "${fg_matches[*]}"
+
+#end=$(( ${#fg_keys[@]} - 1 ))
+#for i in $(seq 0 $end); do
+#    # match each key in the dark-theme file
+#    # parse the foreground color
+#    # add it to the fg_dark_values
+#done
+
+
+#declare -a bg_keys
+#declare -a bg_values
+#process_matches bg_matches bg_keys bg_values "background"
+#
+#end=$(( ${#bg_keys[@]} - 1 ))
+#for i in $(seq 0 $end); do
+#done
+
 	   
 
 # duplicate shiki theme
 #cp leuven.json leuven-dark.json
 
 # replace all original colors with colors from the seleted key matches
+
 
 IFS=$SAVEIFS
