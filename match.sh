93e9b7a318d56745c65db14b91a48e4a8830cf61
strangeloop <strangeloop@thinkpad.local>
2023-10-04 20:31:43 -0400

    Good matches at this point, only a few that aren't found, can manually edit the rest

--- match.sh
+++ match.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+# TODO: Some manual matches when it is not a 1:1 theme variant
+# TODO: Rethink matching, analyze the variables that aren't lining up for clues on how to parse
 if [ "${BASH_VERSINFO[0]}" -lt 4 ] || ([ "${BASH_VERSINFO[0]}" -eq 4 ] && [ "${BASH_VERSINFO[1]}" -lt 3 ]); then
     echo "Script requires bash 4.3+"
     exit 1
@@ -13,8 +15,11 @@ source_file="$1"
 output_file="$2"
 
 # first grep FG and BG colors into arrays
-mapfile -t light_colors_fg < <(grep -i 'foreground":' ./leuven.json)
-mapfile -t light_colors_bg < <(grep -i 'background":' ./leuven.json)
+mapfile -t light_colors_fg < <(grep -i 'foreground":' ./leuven.json | awk '{print tolower($0)}' | awk '!seen[$0]++')
+mapfile -t light_colors_bg < <(grep -i 'background":' ./leuven.json | awk '{print tolower($0)}' | awk '!seen[$0]++')
+echo "Found ${#light_colors_fg[@]} unique foreground colors in source"
+echo "Found ${#light_colors_bg[@]} unique background colors in source"
+
 
 # populate keys of an associated array with the colors for each
 process_matches() {
@@ -22,39 +27,42 @@ process_matches() {
     local -n light_to_dark="$2"
     local fg_or_bg="$3"
     for light_color in "${light_colors[@]}"; do
-	light=$(echo "$light_color" | grep -oE '#[0-9A-Fa-f]{6,8}')
+	light=$(echo "$light_color" | grep -i -oE '#[0-9A-Fa-f]{6,8}')
 	# some shiki color codes
 	light_truncated="${light:0:7}"
 
-	light_theme_match=$(grep -m 1 -E "$fg_or_bg\s*\"$light_truncated\"" ./leuven-theme.el)
-	# if we find a match in the variant theme and haven't already matched this color
-	if [ -n "$light_theme_match" ] && [ -z "${light_to_dark[$light]}" ]; then
-	    # split out the config line so we can use it to search
-	    # the dark variant of the theme
-	    theme_variable="${light_theme_match%%:$fg_or_bg*}"
-	    theme_variable=$(printf "%s" "$theme_variable" | sed 's/[][()\.^$\/\\&*+?|{}]/\\&/g')
-	    # find the corresponding variable in the variant theme
-	    dark_theme_match=$(grep -m 1 -E "$theme_variable" ./leuven-dark-theme.el)
-	    # parse just the dark color code for the variable
-	    dark=$(echo "$dark_theme_match" | grep -oE "$fg_or_bg\s*\"#[0-9A-Fa-f]{6}\"" | grep -oE '#[0-9A-Fa-f]{6}')
-	    # if a color name is found instead of color code, convert it to hex
-	    if [ -z $dark ]; then
+	light_theme_match=$(grep -E -i "$fg_or_bg\s*\"$light_truncated\"" ./leuven-theme.el)
+	if [ -z "$light_theme_match" ]; then
+	    echo "Didn't find a match for $fg_or_bg $light_truncated"
+	    continue
+	fi
 
-		echo "Could not find:"
-		echo "$light_theme_match"
-		echo "Dark match:"
-		echo "$dark_theme_match"
-		dark=$(echo "$dark_theme_match" | grep -oE "$fg_or_bg\s*\".*\"" | grep -oE '"[^"]+"')
-		echo "Found color name: $dark"
-	    fi
-	    # if the source provided AA for alpha at the end, tack it back on for the shiki output
-	    if [ "${#light}" -eq 9 ]; then
-		echo "Adding alpha back"
-		dark+="AA"
-	    fi
-	    if [ -n "$dark" ]; then
-		light_to_dark["$light"]="$dark"
-	    fi
+	if [ -n "${light_to_dark[$light]}" ]; then
+	    echo "Already processed $fg_or_bg $light_truncated - skipping"
+	    continue
+	fi
+
+	# split out the config line so we can use it to search the dark variant of the theme
+	# and remove leading whitespace
+	theme_variable="${light_theme_match#"${light_theme_match%%[![:blank:]]*}"}"
+	# use everything up to the next whitespace as the key
+	theme_variable="${theme_variable%% *}"
+	theme_variable=$(printf "%s" "$theme_variable" | sed 's/[][()\.^$\/\\&*+?|{}]/\\&/g')
+	# find the corresponding variable in the variant theme
+	dark_theme_match=$(grep -m 1 -E "$theme_variable" ./leuven-dark-theme.el)
+	# parse just the dark color code for the variable
+	dark=$(echo "$dark_theme_match" | grep -i -oE "$fg_or_bg\s*\"#[0-9A-Fa-f]{6}\"" | grep -i -oE '#[0-9A-Fa-f]{6}')
+	# if a color isn't found, print error
+	if [ -z $dark ]; then
+	    echo "Could not find $fg_or_bg: [$theme_variable] in variant theme"
+	fi
+	# if the source provided AA for alpha at the end, tack it back on for the shiki output
+	if [ "${#light}" -eq 9 ]; then
+	    dark+="AA"
+	fi
+	if [ -n "$dark" ]; then
+	    echo "Adding $fg_or_bg match for $theme_variable Light: $light Dark: $dark"
+	    light_to_dark["$light"]="$dark"
 	fi
     done
 }
@@ -69,14 +77,18 @@ make_variant() {
 	search_string_light="$fg_or_bg\":\s*\"$light\""
 	replace_string_dark="$fg_or_bg\": \"$dark\""
 	# Replace the colors with the dark variants
-	sed -i 's/'"$search_string_light"'/'"$replace_string_dark"'/' "$output_file"
+	# the first part is case-sensitive to match lowercsat foreground/background
+	# and the second part case-insensitive because we tolowered the colors
+	sed -i "s/\($search_string_light\)/$replace_string_dark/I" "$output_file"
 
 	# capitalize F or B
 	fg_or_bg="${fg_or_bg^}"
 	search_string_light="$fg_or_bg\":\s*\"$light\""
 	replace_string_dark="$fg_or_bg\": \"$dark\""
 	# Replace the colors with the dark variants
-	sed -i 's/'"$search_string_light"'/'"$replace_string_dark"'/' "$output_file"
+	# the first part is case-sensitive to match lowercsat foreground/background
+	# and the second part case-insensitive because we tolowered the colors
+	sed -i "s/\($search_string_light\)/$replace_string_dark/I" "$output_file"
     done
 }
 
@@ -84,6 +96,9 @@ declare -A fg_light_to_dark
 declare -A bg_light_to_dark
 process_matches light_colors_fg fg_light_to_dark "foreground"
 process_matches light_colors_bg bg_light_to_dark "background"
+
+echo "Found ${#fg_light_to_dark[@]} foreground matches"
+echo "Found ${#bg_light_to_dark[@]} background matches"
 
 
 # creating variant of shiki theme
